576e7f49db5a48919be4825fe2e6f4a2cf45c493
Eleonore <el.pc.wald@gmail.com>
2018-12-15 16:51:57 -0500

    Camera follows player, changed background

---
+++ Assets/followPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour {
+    public GameObject player1;
+    public GameObject player2;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player1.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player1.transform.position + offset;
+
+    }
+}
